09bc41979eee672b39ffb8be567f30833901663b
Dmitry Zolotarev <partyzan65@gmail.com>
2021-03-16 20:56:54 +0300

    fixed Err

--- err.go
+++ err.go
@@ -2,8 +2,10 @@ package testutils
 
 import "github.com/stretchr/testify/assert"
 
-func Err(t Tester, _ string, err error) {
-	assert.Equal(t, nil, err)
+func Err(t Tester, name string, err error) {
+	if !assert.Equal(t, nil, err) {
+		t.Errorf("%s: %s", name, err)
+	}
 }
 
 func FatalErr(t Tester, name string, err error) {
